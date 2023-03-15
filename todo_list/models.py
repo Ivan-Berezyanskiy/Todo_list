e38d6bb52457866c207c7896d896e2174eaa40f3
Іван Березянський <mamanadarin@gmail.com>
2023-03-15 17:50:12 +0200

    Add Tag list and static

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 
@@ -7,7 +9,10 @@ class Tag(models.Model):
 
 class Task(models.Model):
     content = models.TextField()
-    datetime = models.DateTimeField(max_length=255)
+    datetime = models.DateTimeField(max_length=255, default=datetime.now())
     deadline = models.DateTimeField(max_length=255)
     is_done = models.BooleanField()
     tag = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["is_done", "-datetime"]
--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from .views import TaskListView
+from .views import TaskListView, TagListView
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="index")
+    path(
+        "",
+        TaskListView.as_view(),
+        name="index"
+    ),
+    path(
+        "tags/",
+        TagListView.as_view(),
+        name="tag_list"
+    ),
 ]
 
 app_name = "todo_list"
